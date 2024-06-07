4455d85755be766af9690b4986dcf22eee25de38
taufik-hdyt <taufikhdyt2599@gmail.com>
2024-06-07 10:45:06 +0700

    fix ads and add doa

---
+++ src/utils/dataTambahan.ts
@@ -0,0 +1,16 @@
+
+
+export const dataTambahan = [
+    {
+        doa: "Doa Agar Cepat Mendapatkan Jodoh",
+        ayat: "رَبِّ لَا تَذَرْنِى فَرْدًا وَأَنتَ خَيْرُ ٱلْوَٰرِثِينَ",
+        latin: "ROBBI LAA TADZARNI FARDAN WA ANTA KHOIRUL WAARITSIN",
+        artinya: "Ya Tuhanku janganlah Engkau membiarkan aku hidup seorang diri dan Engkaulah Waris Yang Paling Baik"
+    },
+    {
+        doa: "Doa memperoleh rezeki yang berkah",
+        ayat: "الْحَمْدُ لِلَّهِ الَّذِي رَزَقَنِي هَذَا مِنْ غَيْرِ حَوْلٍ مِنٌ وَلَا قُوَّةٍ، اللَّهُم",
+        latin: "Alhamdulillaahilladzii razaqanii hadzaa min ghairi haulim minnii walaa quw- watin, allaahumma baarik lii fiihi",
+        artinya: "Artinya: Segala puji bagi Allah, Zat yang telah memberikan rezeki ini dengan tanpa mengeluarkan daya dan kekuatan. Ya Allah.. Berikanlah berkah dalam rezeki yang telah Engkau berikan kepadaku itu"
+    }
+]
